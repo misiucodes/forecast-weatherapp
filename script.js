002654bd035de61a68a1af5c7c714a4d6dd26b79
misiucodes <meeshtran15@gmail.com>
2022-01-06 22:57:18 -0500

    Created a loop in javascript for weekly forecast

--- script.js
+++ script.js
@@ -37,11 +37,45 @@ function showTemperature(response) {
   let showTemp = document.querySelector("#current-temp");
   let temperature = Math.round(response.data.main.temp);
   let cityName = document.querySelector("h3");
+  let tempDescription = document.querySelector("#temp-description");
   let currentWeatherIcon = document.querySelector("#currentWeatherIcon");
+  let windspeed = document.querySelector("#windspeed-units");
+  let humidity = document.querySelector("#humidity-units");
   showTemp.innerHTML = `${temperature}`;
   cityName.innerHTML = `${response.data.name}`;
+  tempDescription.innerHTML = `${response.data.weather[0].description}`;
+  windspeed.innerHTML = `${Math.round(response.data.wind.speed)} km/h`;
+  humidity.innerHTML = `${Math.round(response.data.main.humidity)}%`;
   currentWeatherIcon.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  
+  celciusTemperature = response.data.main.temp;
   console.log(response);
 }
 
+function displayForecast () {
+  let forecastElement = document.querySelector("#forecast");
 
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri"];
+
+  let forecastHTML = `<div class="row d-flex justify-content-evenly">`;
+  days.forEach(function (day) {
+    forecastHTML = forecastHTML + 
+    `
+    <div class="col">
+      <div class="weather-forecast-date">${day}</div>
+      <img src="img/sun.png" class="forecast-icon" width="80px"/>
+          
+        <div class="weekly-temps">
+          <span class="temp-max">-1 / </span>
+          <span class="temp-min"> 2</span>
+        <div class="weather-forecast-description">Cloudy</div>
+      </div>
+    </div>
+    `;
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+displayForecast ();
